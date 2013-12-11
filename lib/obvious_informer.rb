aa8f474df294ea9ca4268f124697639dea906a97
elmar john valmores <elmarjohnvalmores@elmars-Mac-mini.local>
2013-12-11 17:04:18 +0800

    fixed railtie issue

--- lib/obvious_informer.rb
+++ lib/obvious_informer.rb
@@ -30,4 +30,23 @@ module ObviousInformer
 
 end
 
-#include ObviousInformer
+
+if defined?( Rails::Railtie )
+  class ObviousLoggerRailtie < Rails::Railtie
+    initializer 'new_obvious_initializer' do
+
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.send :include, ObviousInformer
+      end
+
+      ActiveSupport.on_load :action_view do
+        ActionView::Base.send :include, ObviousInformer
+      end
+
+      ActiveSupport.on_load :active_record do
+        ActiveRecord::Base.send :include, ObviousInformer
+      end
+
+    end
+  end
+end
